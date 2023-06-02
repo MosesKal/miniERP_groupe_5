dbf0c5885569aac0dc342da65d1781d4c67f364f
MosesK <mosesziongo@gmail.com>
2023-06-02 11:08:40 +0200

    sign in

--- frontend/src/pages/login_page.js
+++ frontend/src/pages/login_page.js
@@ -1,1 +1,13 @@
-import { Button, TextField } from "@mu";
+import React from "react";
+import { Button, TextField } from "@mui/material/";
+
+const login_page = () => {
+  return (
+    <div className="container-login">
+      <div className="login-illustration"></div>
+      <div className="container-formulaire"></div>
+    </div>
+  );
+};
+
+export default login_page;
