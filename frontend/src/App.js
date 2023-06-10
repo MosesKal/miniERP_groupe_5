1c4b788e60b2e51c7ebc1d9ca9114461345e3146
MosesK <mosesziongo@gmail.com>
2023-06-10 20:29:03 +0200

    refont du projet front-end en supprimant les dependances unitiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,11 @@
-import Singup from "./pages/Singup";
-import SingIn from "./pages/SignIn";
+
 import "./styles/App.css";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Stock from "./pages/Stock";
-import Cotation from "./pages/Cotation";
-import Offres from "./pages/Offres.js";
-import Client from "./pages/Client";
-import Comptabilite from "./pages/Comptabilite";
-import Commande from "./pages/Commande";
-import Parametre from "./pages/Parametre";
-import Miningpages from "./pages/Miningpages";
+
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Dashboard/>}></Route>
-              <Route path="/Cotation" element={<Cotation/>}></Route>
-              <Route path="/Offres" element={<Offres/>}></Route>
-              <Route path="/Commande" element={<Commande/>}></Route>
-              <Route path="/Stock" element={<Stock/>}></Route>
-              <Route path="/Comptabilite" element={<Comptabilite/>}></Route>
-              <Route path="/Client" element={<Client/>}></Route>
-              <Route path="/Parametre" element={<Parametre/>}></Route>
-              <Route path="/Miningpages" element={<Miningpages/>}></Route>
-              <Route path="/SingIn" element={<SingIn/>}></Route>
-              <Route path="/Singup" element={<Singup/>}></Route>
-
-          </Routes>
-      </BrowserRouter>
+ 
     </div>
   )
 }
