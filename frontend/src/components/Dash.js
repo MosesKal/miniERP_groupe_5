9ac67e9d2b6e76054b88c9bd5beb53e519a4c07d
MosesK <mosesziongo@gmail.com>
2023-06-14 22:28:31 +0200

    dashboard admin

--- frontend/src/components/Dash.js
+++ frontend/src/components/Dash.js
@@ -1,14 +1,35 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import { Divider, Avatar } from "@mui/material/";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Dash = () => {
   return (
     <div className="dash-container">
       <sidebar className="dash-sideBare">
-        <div className="dash-sideBare_header">Admin Dashboard</div>
+        <div className="dash-sideBare_header">
+          <div className="dash-sideBare_header__content">
+            <h6>Admin Dashboard</h6>
+          </div>
+        </div>
+        <div className="dash-sideBare_nav">
+          <Divider />
+        </div>
+        <div className="dash-sideBare-menu"></div>
       </sidebar>
+
       <main className="dash-main">
-        <div className="header-bar"></div>
+        <div className="header-bar">
+          <div className="header-bar-search">
+            <div className="search-container"></div>
+          </div>
+          <div className="header-bar-avatar">
+            <Avatar
+              alt="Remy Sharp"
+              src=""
+              sx={{ width: 56, height: 56 }}
+            />
+          </div>
+        </div>
       </main>
     </div>
   );
