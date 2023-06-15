b8c137c9b0eae17b462b39f01aa9395e1bcb196e
MosesK <mosesziongo@gmail.com>
2023-06-15 14:30:02 +0200

    dashboard admin

--- frontend/src/components/Dash.js
+++ frontend/src/components/Dash.js
@@ -14,6 +14,7 @@ import FileAriane from "./components/FileAriane";
 import ApartmentIcon from "@mui/icons-material/Apartment";
 import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
 import GroupIcon from "@mui/icons-material/Group";
+import CardStat from "./components/CardStat";
 
 const Dash = () => {
   return (
@@ -73,6 +74,14 @@ const Dash = () => {
           <div className="apercu-2"></div>
           <div className="apercu-3"></div>
           <div className="apercu-4"></div>
+        </div>
+        <div className="titre-dash">
+          <h4>Statistiques</h4>
+        </div>
+        <div className="container-card-stat">
+          <div className="card-stat-1">
+          </div>
+          <div className="card-stat-2"></div>
         </div>
       </main>
     </div>
