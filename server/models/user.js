6b8cdb318aa0982ad4d0ffc0eae06e2e535bc6b3
MosesK <mosesziongo@gmail.com>
2023-06-16 22:35:46 +0200

    ajout des validations dans le modele User

--- server/models/user.js
+++ server/models/user.js
@@ -22,34 +22,77 @@ module.exports = (sequelize, DataTypes) => {
       prenom: {
         type: DataTypes.STRING,
         allowNull: false,
-        entrepriseId: DataTypes.INTEGER,
+        validate: {
+          notNull: {
+            msg: "Le prénom est requis.",
+          },
+        },
       },
       nom: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Le nom est requis.",
+          },
+        },
       },
       role: {
-        type: DataTypes.ENUM("admin", "seler", "mining"),
+        type: DataTypes.ENUM("admin", "seller", "mining"),
         defaultValue: "admin",
+        allowNull: false,
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            msg: "L'email est requis.",
+          },
+          isEmail: {
+            msg: "L'email doit être une adresse email valide.",
+          },
+          async isUniqueEmail(value, { model }) {
+            const user = await model.findOne({ where: { email: value } });
+            if (user) {
+              throw new Error(
+                "Cet email est déjà utilisé par un autre utilisateur."
+              );
+            }
+          },
+        },
       },
       telephone: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            msg: "Le numéro de téléphone est requis.",
+          },
+          isValidPhoneNumber(value) {
+            if (!/^(\+\d{1,3})?\d{6,}$/.test(value)) {
+              throw new Error(
+                "Le numéro de téléphone doit être un numéro valide."
+              );
+            }
+          },
+        },
       },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Le mot de passe est requis.",
+          },
+        },
       },
-      // authTokens: [
-      //   {
-      //     type: String,
-      //     required: true,
-      //   },
-      // ],
+      Tokens: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {
       sequelize,
